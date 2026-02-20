bc4fa3a6d03662b2fc670afcd0e6fa7a862b4a7e
Jitendra Kumar Verma <jitenderkumarv91@gmail.com>
2026-02-20 19:09:01 +0530

    add 3 image

--- html_editors.php
+++ html_editors.php
@@ -9,73 +9,129 @@
 
 </head>
 <body>
-    <h1>HTML Editors:-</h1>
-        <div style="margin-left: 5vh;">
-            <p>A simple text editor is all you need to learn HTML.</p>
+    <nav>
+        <div class="page_name">
+            <img src="logoDesign.png" id="logo">
+            STUDY HERE
         </div>
-
-        <h2>Learn HTML Using Notepad or TextEdit:-</h2>
-        <ul>
-            <li>Web pages can be created and modified by using professional HTML editors.</li>
-            <li>However, for learning HTML we recommend a simple text editor like Notepad (PC) or TextEdit (Mac).</li>
-            <li>We believe that using a simple text editor is a good way to learn HTML.</li>
-            <li>Follow the steps below to create your first web page with Notepad or TextEdit.</li>
-        </ul>
-        <h1>Step 1: Open Notepad (PC)</h1>
-    <b>Windows 8 or later:</b> <br/>
-
-    Open the <b>Start Screen</b> (the window symbol at the bottom left on your screen). <b>Type Notepad</b>.<br/>
-    <b>
-    Windows 7 or earlier:</b><br/>
-
-    Open Start > Programs > Accessories > Notepad
-
-    <h1>Step 1: Open TextEdit (mac)</h1>
-    Open<b>Finder > Applications > TextEdit</b> <br/>
-
-    Also change some preferences to get the application to save files correctly.In <b>Preferences > Format ></b> choose <b>"Plain Text"</b><br/>
-
-    Then under "Open and Save", check the box that says "Display HTML files as HTML code instead of formatted text".<br/>
-
-    <b>Then open a new document to place the code.</b>
-    <h1>Step 2: Write Some HTML</h1>
-    <b>Write or copy the following HTML code into Notepad:</b><br/>
-    <br/>
-<div style="width: 59vh; height: 25vh; border: solid rgba(128, 128, 128, 0.438) 20px;padding:10px;">
-    <div class="w3-code notranslate htmlHigh">
-    <span class="doctypecolor" style="color:slategray">&lt;!DOCTYPE html&gt;</span><br>
-    <span class="tagnamecolor" style="color:#905"><span class="tagcolor" style="color:#999">&lt;</span>
-    html<span class="tagcolor" style="color:#999">&gt;</span></span><br>
-    <span class="tagnamecolor" style="color:#905"><span class="tagcolor" style="color:#999">&lt;</span>
-    body<span class="tagcolor" style="color:#999">&gt;</span></span><br><br><span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>h1<span class="tagcolor" style="color:#999">&gt;</span>
-    </span>My First Heading<span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>/h1<span class="tagcolor" style="color:#999">&gt;</span></span><br><br>
-    <span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>p<span class="tagcolor" style="color:#999">&gt;</span>
-    </span>My first paragraph.<span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>/p<span class="tagcolor" style="color:#999">&gt;</span>
-    </span><br><br><span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>/body<span class="tagcolor" style="color:#999">&gt;</span>
-    </span><br><span class="tagnamecolor" style="color:#905">
-    <span class="tagcolor" style="color:#999">&lt;</span>/html<span class="tagcolor" style="color:#999">&gt;</span></span> </div>
-</div><br/>
-    <div>
-        <img src="img_notepad.png" />
+        <div class="multiple_div">
+        <div class="product_">
+            <a id="product_1" href="">HOME</a><!--home page-->
+        </div>
+        <div class="resource_">
+            <a id="resource_1" href="">COURSE</a><!--COURSE NAME-->
+        </div>
+        <div class="team_">
+            <a id="team_1" href="">CURTIFICATE</a><!--teachers name-->
+        </div>
+        <div class="help_">
+            <a id="help_1" href="">AUTHOR</a><!--AUTHOR-->
+        </div>
+        <div class="community_">
+            <a id="community_1" href="">ABOUT</a><!--ABOUT-->
+        </div>
+        <div class="about_">
+            <a id="about_1" href="">PROFILE</a>
+        </div>
+        </div>
+        <div class="login">
+            <a id="login_1" href="login.html" onclick="OpenLogin()">Login</a>
+        
+            <a id="singup_1" href="singin.html">Sing up</a>
+        </div>
+    </nav>
+    <div class="searchBar">
+        <input type="text" placeholder="Search Here" >
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+            </svg>
+        </button>
     </div>
-    <h1>Step 3: Save the HTML Page</h1>
-    Save the file on your computer. Select <b>File > Save as</b> in the Notepad menu.<br/><br/>
-    Name the file <b>"index.htm"</b> set the encoding to <b>UTF-8</b> (which is the preferred encoding for HTML files).<br/>
-    <br/>
-    <div>
-        <img src="save_file.png">
+    <div class="cssMaincontant">
+        <div class="manuBar">
+        <h1>HTML Tutorial</h1>
+            <a href="html_introduction.php">Html Introduction</a><br/>
+            <a href="html_editors.php">html_editors</a><br/>
+            <a href="selector.php"></a><br/>
+            <a href="how-to.html">CSS How-to-Add</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+            <a href="">s</a><br/>
+        </div>
+    <div class="cssDiv">
+        <h1>HTML Editors:-</h1>
+            <div style="margin-left: 5vh;">
+                <p>A simple text editor is all you need to learn HTML.</p>
+            </div>
+
+            <h2>Learn HTML Using Notepad or TextEdit:-</h2>
+            <ul>
+                <li>Web pages can be created and modified by using professional HTML editors.</li>
+                <li>However, for learning HTML we recommend a simple text editor like Notepad (PC) or TextEdit (Mac).</li>
+                <li>We believe that using a simple text editor is a good way to learn HTML.</li>
+                <li>Follow the steps below to create your first web page with Notepad or TextEdit.</li>
+            </ul>
+            <h1>Step 1: Open Notepad (PC)</h1>
+        <b>Windows 8 or later:</b> <br/>
+
+        Open the <b>Start Screen</b> (the window symbol at the bottom left on your screen). <b>Type Notepad</b>.<br/>
+        <b>
+        Windows 7 or earlier:</b><br/>
+
+        Open Start > Programs > Accessories > Notepad
+
+        <h1>Step 1: Open TextEdit (mac)</h1>
+        Open<b>Finder > Applications > TextEdit</b> <br/>
+
+        Also change some preferences to get the application to save files correctly.In <b>Preferences > Format ></b> choose <b>"Plain Text"</b><br/>
+
+        Then under "Open and Save", check the box that says "Display HTML files as HTML code instead of formatted text".<br/>
+
+        <b>Then open a new document to place the code.</b>
+        <h1>Step 2: Write Some HTML</h1>
+        <b>Write or copy the following HTML code into Notepad:</b><br/>
+        <br/>
+    <div style="width: 59vh; height: 30vh; border: solid rgba(128, 128, 128, 0.438) 20px;padding:10px;">
+        <div class="w3-code notranslate htmlHigh">
+        <span class="doctypecolor" style="color:slategray">&lt;!DOCTYPE html&gt;</span><br>
+        <span class="tagnamecolor" style="color:#905"><span class="tagcolor" style="color:#999">&lt;</span>
+        html<span class="tagcolor" style="color:#999">&gt;</span></span><br>
+        <span class="tagnamecolor" style="color:#905"><span class="tagcolor" style="color:#999">&lt;</span>
+        body<span class="tagcolor" style="color:#999">&gt;</span></span><br><br><span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>h1<span class="tagcolor" style="color:#999">&gt;</span>
+        </span>My First Heading<span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>/h1<span class="tagcolor" style="color:#999">&gt;</span></span><br><br>
+        <span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>p<span class="tagcolor" style="color:#999">&gt;</span>
+        </span>My first paragraph.<span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>/p<span class="tagcolor" style="color:#999">&gt;</span>
+        </span><br><br><span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>/body<span class="tagcolor" style="color:#999">&gt;</span>
+        </span><br><span class="tagnamecolor" style="color:#905">
+        <span class="tagcolor" style="color:#999">&lt;</span>/html<span class="tagcolor" style="color:#999">&gt;</span></span> </div>
     </div><br/>
-    <b>Tip:</b>You can use either .htm or .html as file extension. There is no difference; it is up to you.
-    <h1>Step 4: View the HTML Page in Your Browser</h1>
-    Open the saved HTML file in your favorite browser (double click on the file, or right-click - and choose "Open with").<br/><br/>
-    The result will look much like this:
-    <div>
-        <img src="img_index.png" alt="">
+        <div>
+            <img src="img_notepad.png" />
+        </div>
+        <h1>Step 3: Save the HTML Page</h1>
+        Save the file on your computer. Select <b>File > Save as</b> in the Notepad menu.<br/><br/>
+        Name the file <b>"index.htm"</b> set the encoding to <b>UTF-8</b> (which is the preferred encoding for HTML files).<br/>
+        <br/>
+        <div>
+            <img src="save_file.png">
+        </div><br/>
+        <b>Tip:</b>You can use either .htm or .html as file extension. There is no difference; it is up to you.
+        <h1>Step 4: View the HTML Page in Your Browser</h1>
+        Open the saved HTML file in your favorite browser (double click on the file, or right-click - and choose "Open with").<br/><br/>
+        The result will look much like this:<br/><br/>
+        <div style="margin-left:10%;">
+            <img src="img_index.png" alt="">
+        </div>
     </div>
 </body>
 </html>
